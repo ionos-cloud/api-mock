143b723eace35e3dacd22b0e669968817ffd5997
Florin Mihalache <florin.mihalache@ionos.com>
2021-01-11 11:10:09 +0200

    unit tests updated

--- test/utils/sandbox.test.ts
+++ test/utils/sandbox.test.ts
@@ -1,22 +1,26 @@
-import { sandboxRun } from '../../src/utils/sandbox'
+import { Sandbox } from '../../src/utils/sandbox'
 import { assert } from 'chai'
 import registry from '../../src/services/symbol-registry'
 
 describe('sandbox run works', function () {
   it('should evaluate booleans', function () {
-    assert.equal(sandboxRun('return foo === "abc"', {foo: 'abc'}), true)
+    const sandbox = new Sandbox({foo: 'abc'})
+    assert.equal(sandbox.run('return foo === "abc"'), true)
   })
 
   it('should correctly access sandbox data', function () {
-    assert.equal(sandboxRun('return a + b', {a: 1, b: 2}), 3)
+    const sandbox = new Sandbox({a: 1, b: 2})
+    assert.equal(sandbox.run('return a + b'), 3)
   })
 
   it('should have access to the registry', function () {
     registry.save('test', {a: 1, b: 2})
-    assert.equal(sandboxRun('return test.a + test.b'), 3)
+    const sandbox = new Sandbox({})
+    assert.equal(sandbox.run('return test.a + test.b'), 3)
   })
 
   it('should not access global scope', function () {
-    assert.equal(sandboxRun('return sandboxRun'), undefined)
+    const sandbox = new Sandbox({})
+    assert.equal(sandbox.run('return sandboxRun'), undefined)
   })
 })
